1dad8f63e3edb5f8789541b4ad54674f23259f82
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-08-04 19:35:50 +0900

    feat: multiLine 그려보기

--- canvas-script.js
+++ canvas-script.js
@@ -1,11 +1,27 @@
 window.onload = function () {
-  const canvas = document.getElementById("line-canvas");
+  const canvas = document.getElementById("multi-line-canvas");
   const context = canvas.getContext("2d");
 
   context.beginPath();
-  context.strokeStyle = "red";
-  context.lineWidth = 10;
-  context.moveTo(30, 70);
-  context.lineTo(130, 70);
+  context.moveTo(30, 30);
+  context.lineTo(80, 80);
+  context.lineTo(130, 30);
+  context.lineTo(180, 80);
+  context.lineTo(230, 30);
   context.stroke();
+
+  //   context.beginPath();
+  //   context.moveTo(80, 80);
+  //   context.lineTo(130, 30);
+  //   context.stroke();
+
+  //   context.beginPath();
+  //   context.moveTo(130, 30);
+  //   context.lineTo(180, 80);
+  //   context.stroke();
+
+  //   context.beginPath();
+  //   context.moveTo(180, 80);
+  //   context.lineTo(230, 30);
+  //   context.stroke();
 };
